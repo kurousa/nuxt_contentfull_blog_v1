2316c60432c70f6c2422c3468ca445ddf0a6ccac
kurousa <r.takahashi.1026@gmail.com>
2021-04-28 23:40:54 +0900

    add_middleware_by_getContentful.js

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -36,21 +36,20 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import client from '~/plugins/contentful'
 import draftChip from '~/components/posts/draftChip.vue'
 
 export default {
   components: {
     draftChip
   },
-  async asyncData({ env, params }) {
-    let currentPost = null
-    await client.getEntries({
-      content_type: env.CTF_BLOG_POST_TYPE_ID,
-      'fields.slug': params.slug
-    }).then(res => (currentPost = res.items[0])).catch(console.error)
+  async asyncData({ payload, store, params, error }) {
+    const currentPost = payload || await store.state.posts.find(post => post.fields.slug === params.slug)
 
-    return { currentPost }
+    if (currentPost) {
+      return { currentPost }
+    } else {
+      return error({ statusCode: 400 })
+    }
   },
   computed: {
     ...mapGetters(['setEyeCatch', 'draftChip'])
